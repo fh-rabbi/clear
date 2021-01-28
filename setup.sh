99a5412c029676ccfae5d2c0e9a011410f08fafc
Fazle Rabbi <71178740+fh-rabbi@users.noreply.github.com>
2021-01-28 17:40:41 +0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,8 @@
+cd $HOME
+clear
+pkg install python -y
+mv p /data/data/com.termux/files/usr/bin
+cd /data/data/com.termux/files/usr/bin
+chmod +x p
+cd $HOME
+echo -e "\e[32mNow type p command and clear your terminal"
